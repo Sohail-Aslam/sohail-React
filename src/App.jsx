c257ebaa6fc290cf3751931b9623f4676d590f78
Sohail Aslam <you@example.com>
2024-07-30 12:45:51 +0500

    Todo app using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { Canvas } from 'react'
+import "./App.css"
+import { Mesh } from 'three';
 
-let id = 0
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
 
-const Todo = props => (
-  <li>
-    <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle} />
-    <button onClick={props.onDelete}>delete</button>
-    <span>{props.todo.text}</span>
-  </li>
-)
+  function handleChange(e) {
+    setInputValue(e.target.value);
+  }
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [],
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (editIndex !== null) {
+      // Edit existing todo
+      const updatedTodos = todos.map((todo, index) =>
+        index === editIndex ? { ...todo, text: inputValue } : todo
+      );
+      setTodos(updatedTodos);
+      setEditIndex(null);
+    } else {
+      // Add new todo
+      setTodos([...todos, { text: inputValue, completed: false }]);
     }
+    setInputValue('');
   }
 
-  addTodo() {
-    const text = prompt("Todo text please");
-    this.setState({
-      todos: [...this.state.todos, { id: id++, text: text, checked: false }],
-    })
+  function handleEdit(index) {
+    setInputValue(todos[index].text);
+    setEditIndex(index);
   }
 
-  removeTodo(id) {
-    this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id)
-    })
-  }
-  toggleTodo(id) {
-    this.setstate({
-      todos: this.state.todos.map(todo => {
-        if (todo.id !== id) return todo
-        return {
-          id: todo.id,
-          text: todo.text,
-          checked: !todo.checked,
-        }
-      })
-    })
+  // function handleRemove(index) {
+  //   const updatedTodos = todos.filter((_, i) => i !== index);
+  //   setTodos(updatedTodos);
+  // }
+
+  function handleToggleCompleted(index) {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
   }
 
-
-  render() {
-    return (
-      <div>
-        <div>Todo count: {this.state.todos.length}</div>
-        <div>Unchecked todo count: {this.state.todos.filter(todo => !todo.checked).length}</div>
-        <button onClick={() => this.addTodo()}>Add Todo</button>
-        <ul>
-          {this.state.todos.map(todo => (
-            <Todo
-              onToggle={() => this.toggleTodo(todo.id)}
-              onDelete={() => this.removeTodo(todo.id)}
-              todo={todo} />
-          ))}
-        </ul>
-      </div>
-    )
+  function handleDeleteCompleted() {
+    const updatedTodos = todos.filter(todo => !todo.completed);
+    setTodos(updatedTodos);
   }
+
+  return (
+    <div className='container'>
+      <mesh>
+        <boxBufferGeometry atach="geometry" />
+        <meshLandbortMaterial attach='material' color='red' />
+      </mesh>
+      <h1>Todo List</h1>
+      <button className='btn' onClick={handleDeleteCompleted}>Delete Completed</button>
+      <form onSubmit={handleSubmit}>
+        <input placeholder='What the task today?' className='input' type='text' value={inputValue} onChange={handleChange} />
+        <button className='btn' type='submit'>{editIndex !== null ? 'Update Todo' : 'Add Todo'}</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li className='list' key={index} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            <div>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleToggleCompleted(index)}
+              />
+            </div>
+            {todo.text}
+            <div className='btns'>
+              <button className='btn edit' onClick={() => handleEdit(index)}>Edit</button>
+
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-export default App
+
+export default TodoApp;
